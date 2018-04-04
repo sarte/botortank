639c8f8b4fcf9fef30f0095766b3a566a9192f28
MEIDI <meidi.garcia@student.uclouvain.be>
2018-04-04 22:08:56 +0200

    Essai controller

--- botortank_wk/src/bot/src/structure.h
+++ botortank_wk/src/bot/src/structure.h
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 typedef struct Struct
 {
-	int count;
-
+	double int_error_1,int_error_2,int_error_3,int_error_4;
+	double wheel_commands1,wheel_commands2,wheel_commands3,wheel_commands4;
 } Struct;
 
 Struct* init_Struct();
